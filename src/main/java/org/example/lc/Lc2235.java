1e57bbe3178a5e078527287a7c61a7fd76910217
Black <l441900894@yeah.net>
2025-08-02 14:12:41 +0800

    lc2235

---
+++ src/main/java/org/example/lc/Lc2235.java
@@ -0,0 +1,11 @@
+package org.example.lc;
+
+public class Lc2235 {
+    public static void main(String[] args) {
+
+    }
+
+    public int sum(int num1, int num2) {
+        return num1+num2;
+    }
+}
