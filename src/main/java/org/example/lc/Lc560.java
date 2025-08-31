590af840483dc0fd3aa4ceda4d96cad1d76cf46f
Black <l441900894@yeah.net>
2025-08-31 21:59:12 +0800

    lc560

--- src/main/java/org/example/lc/Lc560.java
+++ src/main/java/org/example/lc/Lc560.java
@@ -2,37 +2,23 @@ package org.example.lc;
 
 public class Lc560 {
     public static void main(String[] args) {
-        int[] a = {-1,-1,1};
-        int i = subarraySum(a, 6);
+        int[] a = {1,1,1};
+        int i = subarraySum(a, 2);
         System.out.println(i);
     }
 
     public static int subarraySum(int[] nums, int k) {
-        // todo not done
+        int[] prefixArr = new int[nums.length + 1];
+        prefixArr[1] = nums[0];
+        for (int i = 2; i < prefixArr.length; i++) {
+            prefixArr[i] = prefixArr[i - 1] + nums[i - 1];
+        }
         int count = 0;
-        int left = 0;
-        int right = 0;
-        int sum = 0;
-        while (right < nums.length && left < nums.length) {
-            if (left >= right) {
-                right = left;
-                sum = nums[left];
-            } else {
-//                sum += nums[right];
-            }
-            if (sum == k) {
-                sum -= nums[left];
-                left++;
-                count++;
-            } else if (sum < k) {
-                right++;
-                if (right >= nums.length) {
-                    break;
+        for (int i = 0; i < prefixArr.length; i++) {
+            for (int j = i + 1; j < prefixArr.length; j++) {
+                if (prefixArr[j] - prefixArr[i] == k) {
+                    count++;
                 }
-                sum += nums[right];
-            } else {
-                sum -= nums[left];
-                left++;
             }
         }
         return count;
