fa22e4cf7329e13e33a80a1477bdf5e839e89729
Black <l441900894@yeah.net>
2025-09-03 20:55:43 +0800

    lc206 反转链表

---
+++ src/main/java/org/example/lc/Lc206.java
@@ -0,0 +1,34 @@
+package org.example.lc;
+
+public class Lc206 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    public static void main(String[] args) {
+        ListNode n5 = new ListNode(5);
+        ListNode n4 = new ListNode(4, n5);
+        ListNode n3 = new ListNode(3, n4);
+        ListNode n2 = new ListNode(2, n3);
+        ListNode n1 = new ListNode(1, n2);
+        ListNode listNode = reverseList(n1);
+        System.out.println(listNode.val);
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
