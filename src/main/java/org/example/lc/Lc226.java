547587f1492f61f2c30f06c83a2c2f46e64ca14e
Black <l441900894@yeah.net>
2025-09-13 21:10:47 +0800

    lc226 翻转二叉树

---
+++ src/main/java/org/example/lc/Lc226.java
@@ -0,0 +1,16 @@
+package org.example.lc;
+
+import org.example.lc.model.TreeNode;
+
+public class Lc226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}
